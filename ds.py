726dc083de703bd65815d4a22fc2abeb3924b3dd
MohamedElshewy <abdalluhmohamed@gmail.com>
2025-12-02 20:26:02 +0200

    Add peer computation and available options methods to SudokuBoard class

--- ds.py
+++ ds.py
@@ -1,4 +1,6 @@
 class SudokoBoard:
+    peers = []
+
     def __init__(self, initial_state: str = None):
         self.grid = [0] * 81
         self.domains = [0b111111111] * 81
@@ -31,3 +33,37 @@ class SudokoBoard:
         mask = (1 << (value - 1))
         return (self.domains[idx] & mask) != 0
 
+    def available_options(self, r, c):
+        ops = []
+        for i in range(1, 10):
+            if self.is_value_in_domain(r, c, i):
+                ops.append(i)
+        return ops
+
+    @classmethod
+    def compute_peers(cls):
+        cls.peers = [set() for _ in range(81)]
+        for idx in range(81):
+            row = idx // 9
+            col = idx % 9
+            # in the same row
+            for c in range(9):
+                cls.peers[idx].add(row * 9 + c)
+
+            # in the same col
+            for r in range(9):
+                cls.peers[idx].add(r * 9 + col)
+
+            # same box
+            box_r = (row // 3) * 3
+            box_c = (col // 3) * 3
+            for r in range(3):
+                for c in range(3):
+                    cls.peers[idx].add((box_r + r) * 9 + (box_c + c))
+
+            cls.peers[idx].discard(idx)
+
+        cls.peers = [list(p) for p in cls.peers]
+
+
+SudokoBoard.compute_peers()
